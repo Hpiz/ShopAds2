26e54849c88a8309646b98123e6cf2d36fa238d5
unknown <Chris@Chris-PC.(none)>
2011-12-15 19:06:13 -0500

    Increased efficiency of list command

--- org/hpiz/ShopAds2/Util/Messaging/CommandMessage.java
+++ org/hpiz/ShopAds2/Util/Messaging/CommandMessage.java
@@ -89,25 +89,26 @@ public class CommandMessage extends ShopAdsMessage {
         }
         player.sendMessage(prefix + "These are all the current ads.");
         for (Shop s : shops) {
-            player.sendMessage(config.getLabelColor() + "[" + s.getShopName() + "] " + config.getMessageColor() + s.getAd());
+            if (s.shopAdvertising() && !s.shopExpired()) {
+                player.sendMessage(config.getLabelColor() + "[" + s.getShopName() + "] " + config.getMessageColor() + s.getAd());
+            }
         }
     }
-    
-     public void listShops(Player player) {
+
+    public void listShops(Player player) {
         if (shops == null || shops.length < 1) {
             player.sendMessage(prefix + "There are no shops available to teleport to.");
             return;
         }
         player.sendMessage(prefix + "These are all the current shops.");
         for (Shop s : shops) {
-            if (s.advertisesIn(player.getWorld().getName())){
-                if (s.getWorld()== player.getWorld()){
+            if (s.shopAdvertising() && !s.shopExpired() && s.advertisesIn(player.getWorld().getName())) {
+                if (s.getWorld() == player.getWorld()) {
                     player.sendMessage(config.getLabelColor() + s.getShopName());
-                }else {
+                } else {
                     player.sendMessage(config.getLabelColor() + s.getShopName() + "*");
                 }
             }
-            
         }
     }
 }
