9d0495f9bdd1209caefb0642033a525fd706318c
unknown <Chris@Chris-PC.(none)>
2011-12-16 23:52:38 -0500

    Converted shops to ArrayList for cleaner code and improved efficiency

--- org/hpiz/ShopAds2/Shop/ShopHandler.java
+++ org/hpiz/ShopAds2/Shop/ShopHandler.java
@@ -4,7 +4,9 @@
  */
 package org.hpiz.ShopAds2.Shop;
 
+import java.util.ArrayList;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.hpiz.ShopAds2.ShopAds2;
 import org.hpiz.ShopAds2.Util.ServerInterface;
 
@@ -13,88 +15,58 @@ import org.hpiz.ShopAds2.Util.ServerInterface;
  * @author Chris
  */
 public class ShopHandler extends ShopAds2 {
-
+    private ArrayList<Shop> shops = new ArrayList<Shop>();
     public void addShop(Shop shop) {
-        message.console.debug("ShopHandler addshop");
-        if (shops != null && shops.length > 0) {
-            message.console.debug("some shops already exist");
-            Shop[] transfer = new Shop[shops.length + 1];
-            for (int i = 0; i < shops.length; i++) {
-                transfer[i] = shops[i];
-            }
-            transfer[shops.length] = shop;
-            shops = new Shop[transfer.length];
-            shops = transfer;
-            playerHandler.getPlayer(shop.getShopOwner()).addOwnedShop();
-            return;
-        }
-        message.console.debug ("no shops already existed");
-        shops = new Shop[1];
-        shops[0] = shop;
+        shops.add(shop);
         playerHandler.getPlayer(shop.getShopOwner()).addOwnedShop();
-        message.console.debug("Shop -" + shops[0].getShopName() + " was created");
     }
 
     public void removeShop(Shop shop) {
-        if (shops != null && shops.length > 1) {
-            if (shopExists(shop)) {
-                Shop[] transfer = new Shop[shops.length - 1];
-                int index = getShopIndex(shop);
-                for (int i = 0; i < index; i++) {
-                    transfer[i] = shops[i];
-                }
-                for (int i = index + 1; i < shops.length; i++) {
-                    transfer[i - 1] = shops[i];
-                }
-                shops = new Shop[transfer.length];
-                shops = transfer;
-            }
-        }
-        shops = null;
+        shops.remove(shop);
+        playerHandler.getPlayer(shop.getShopOwner()).addOwnedShop();
 
     }
 
-    public Shop getShopByName(String name) {
-        if (shops != null && shops.length > 0) {
-            for (int i = 0; i < shops.length; i++) {
-                if (shops[i].getShopName().equalsIgnoreCase(name)) {
-                    return shops[i];
+    public Shop getShop(String name) {
+        Shop[] temp = new Shop[shops.size()];
+        shops.toArray(temp);
+        if (temp != null && temp.length > 0) {
+            for (int i = 0; i < temp.length; i++) {
+                if (temp[i].getShopName().equalsIgnoreCase(name)) {
+                    return temp[i];
                 }
             }
         }
         return null;
 
     }
+    
+    public boolean shopsEmpty(){
+        return shops.isEmpty();
+    }
 
     public boolean shopExists(Shop shop) {
-        if (shops != null && shops.length > 0) {
-            for (Shop test : shops) {
-                if (shop == test) {
-                    return true;
-                }
-            }
+        if(shops.contains(shop)){
+            return true;
         }
         return false;
     }
     
     public boolean shopExists(String shop){
-        for(Shop test :shops){
+        Shop[]temp = new Shop[shops.size()];
+        shops.toArray(temp);
+        if (temp != null && temp.length > 0) {
+        for(Shop test :temp){
             if(test.getShopName().equalsIgnoreCase(shop)){
                 return true;
             }
+        }
         }
         return false;
     }
 
     public int getShopIndex(Shop shop) {
-        if (shops != null && shops.length > 0) {
-            for (int i = 0; i < shops.length; i++) {
-                if (shop == shops[i]) {
-                    return i;
-                }
-            }
-        }
-        return -1;
+        return shops.indexOf(shop);
     }
 
     public void addWorldToShop(Shop shop, String string) {
@@ -102,6 +74,25 @@ public class ShopHandler extends ShopAds2 {
             return;
         }
         shop.addWorldToAdvertiseIn(serverInterface.getWorld(string));
+    }
+
+    public boolean ownsShop(Shop shop, Player player) {
+        if (player.getName()==shop.getShopOwner()){
+            return true;
+        }
+        return false;
+    }
+
+    public ArrayList<Shop> getShops() {
+        return shops;
+    }
+
+    public int getSize() {
+        return shops.size();
+    }
+
+    public Shop getShop(int index) {
+        return shops.get(index);
     }
     
 }
