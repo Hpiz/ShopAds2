b660443ab77afde9b9bbacde8b31d2685a431b13
unknown <Chris@Chris-PC.(none)>
2011-12-16 23:53:42 -0500

    Renamed class

---
+++ org/hpiz/ShopAds2/Util/ArrayFunctions.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.hpiz.ShopAds2.Util;
+
+import org.hpiz.ShopAds2.ShopAds2;
+
+/**
+ *
+ * @author Chris
+ */
+public class ArrayFunctions extends ShopAds2{
+    public Object[] addToArray (Object[] O, Object o){
+        if (O != null && O.length > 0) {
+            Object[] transfer = new Object[O.length+ 1];
+            for (int i = 0; i < O.length; i++) {
+                transfer[i] = O[i];
+            }
+            transfer[O.length] = o;
+            O = new Object[transfer.length];
+            O = transfer;
+            return O;
+        }
+        O = new Object[1];
+        O[0] = o;
+        return O;
+       
+        
+    
+}
+}
